7dd80e900ad13347391fe3a5261d5540752dfc18
aadilparmar <124196983+aadilparmar@users.noreply.github.com>
2025-09-09 20:35:49 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/deploymentTargetSelector.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/menu/bottom_nav_menu.xml

--- app/src/main/java/com/example/cryptocritic/MainActivity.kt
+++ app/src/main/java/com/example/cryptocritic/MainActivity.kt
@@ -11,19 +11,19 @@ import androidx.navigation.fragment.findNavController
 import com.example.cryptocritic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private  lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
         val navController = navHostFragment!!.findNavController()
-        val popupMenu = PopupMenu(this,null)
+        val popupMenu = PopupMenu(this,binding.bottomBar)
         popupMenu.inflate(R.menu.bottom_nav_menu)
         binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
